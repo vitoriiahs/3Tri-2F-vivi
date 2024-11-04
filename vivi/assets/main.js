3545cb1bdc19371b9a7395b5ee8fe38e0a7953a1
vitoriiahs <160124644+vitoriiahs@users.noreply.github.com>
2024-11-04 11:45:22 -0300

    Add files via upload

--- vivi/assets/main.js
+++ vivi/assets/main.js
@@ -1,3 +1,21 @@
 function criaCartao(tema,pergunta,resposta){
+    const container = document.getElementById('container');
+    let cartao = document.createElement ('article');
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+             <div class="cartao-conteudo">
+             <h3>${tema}</h3>
+             <div class="cartao-conteudo-pergunta">
+             ${pergunta}
+              </div>
+             < class="cartao-conteudo-resposta">
+             ${resposta}
+             </div>
+             </div>
     
+    `
+    container.appendChild(cartao)
 }
+criaCartao('Historia','Em que ano iniciou-se a guerra do Vietna?','Em 1955')
+
+criaCartao('Portugues','O que e uma paroxitona?','Uma paroxitona e uma palavra cuja silaba tonica e penultima')
