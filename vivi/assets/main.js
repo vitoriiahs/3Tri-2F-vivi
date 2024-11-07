ae1ea3a30b34d9eb90dcb2a58638c9aecfe78fc8
vitoriiahs <160124644+vitoriiahs@users.noreply.github.com>
2024-11-07 12:29:00 -0300

    Add files via upload

--- vivi/assets/main.js
+++ vivi/assets/main.js
@@ -1,21 +1,19 @@
 function criaCartao(tema,pergunta,resposta){
     const container = document.getElementById('container');
-    let cartao = document.createElement ('article');
-    cartao.className = 'cartao'
-    cartao.innerHTML = `
-             <div class="cartao-conteudo">
-             <h3>${tema}</h3>
-             <div class="cartao-conteudo-pergunta">
-             ${pergunta}
-              </div>
-             < class="cartao-conteudo-resposta">
-             ${resposta}
-             </div>
-             </div>
-    
+    let cartao = document.createElement('article');
+    cartao.className =  'cartao';
+    cartao.innerHTML =`
+    <div class="cartao-conteudo">
+    <h3>${tema}</h3>
+    <div class="cartao-conteudo-pergunta">
+        ${pergunta}
+    </div>
+    <div class="cartao-conteudo-resposta">
+        ${resposta}
+    </div>
+    </div>
     `
     container.appendChild(cartao)
-}
-criaCartao('Historia','Em que ano iniciou-se a guerra do Vietna?','Em 1955')
-
-criaCartao('Portugues','O que e uma paroxitona?','Uma paroxitona e uma palavra cuja silaba tonica e penultima')
+    }
+    criaCartao('historia','em que ano iniciou-se a guerra do vietnã?','Em 1955')
+    criaCartao('portugues','oque é paroxitona?','uma paroxitona é uma palavra cuja silaba tonica é penultima')
